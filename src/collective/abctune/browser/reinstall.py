b882fd865c7e476c63c42370143066f05196f2db
Eric Hardy <Eric.Hardy@univ-brest.fr>
2017-11-04 17:43:01 +0100

    misc

---
+++ src/collective/abctune/browser/reinstall.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from plone import api
+from plone.protect.interfaces import IDisableCSRFProtection
+from Products.Five import BrowserView
+from zope.interface import alsoProvides
+
+
+class reinstall(BrowserView):
+    def __call__(self):
+        alsoProvides(self.request, IDisableCSRFProtection)
+        site = api.portal.get()
+        qi = site.restrictedTraverse('portal_quickinstaller')
+        # qi.reinstallProducts('collective.abctransforms')
+        qi.uninstallProducts(['collective.abctune'])
+        qi.installProduct('collective.abctune')
+        qi.reinstallProducts('collective.abctransforms')
+        url = self.context.absolute_url()
+        self.request.response.redirect(url)
