4a5f29f29b802f51794665981c9438aeb8441e76
Eric Hardy <Eric.Hardy@univ-brest.fr>
2017-12-12 15:39:40 +0100

    viewlet menu

---
+++ src/collective/abctune/browser/viewlets/abctune_menu.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from plone.app.layout.viewlets import common as base
+
+import logging
+
+
+logger = logging.getLogger('abctuneMenu')
+
+
+class abctuneMenu(base.ViewletBase):
+    pass
---
+++ src/collective/abctune/browser/viewlets/interfaces.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from zope.viewlet.interfaces import IViewletManager
+
+
+class IABCTuneViewlets(IViewletManager):
+    """ A viewlet manager that sits for HOME view
+    """
