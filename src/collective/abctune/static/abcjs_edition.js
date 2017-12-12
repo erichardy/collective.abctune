4ffa826321a18f65aa0542902241c3fc3177f8b0
Eric Hardy <Eric.Hardy@univ-brest.fr>
2017-12-12 15:10:10 +0100

    test right menu in abc view

--- src/collective/abctune/static/abcjs_edition.js
+++ src/collective/abctune/static/abcjs_edition.js
@@ -1,12 +1,47 @@
 /*
 */
-
-console.log('dans EDition');
-console.log(ABCJS);
-
 $(document).ready(function(){
+	console.log('dans EDition');
+	console.log(ABCJS);
+
+	/* https://www.w3schools.com/howto/howto_js_off-canvas.asp */
+	function openOffcanvas() {
+	    document.getElementById("myOffcanvas").style.width = "250px";
+	    document.getElementById("main-container").style.marginRight = "250px";
+	};
+	function openNav3() {
+	    document.getElementById("myCanvasNav").style.width = "100%";
+	    document.getElementById("myCanvasNav").style.opacity = "0.8";  
+	};
+	function closeOffcanvas() {
+	    document.getElementById("myOffcanvas").style.width = "0%";
+	    document.getElementById("main-container").style.marginRight= "0%";
+	    document.body.style.backgroundColor = "white";
+	    document.getElementById("myCanvasNav").style.width = "0%";
+	    document.getElementById("myCanvasNav").style.opacity = "0"; 
+	};
+	function closeOffcanvas2() {
+	    document.getElementById("myOffcanvas2").style.display = "none";
+	};
+	$("#dashboard").click(function(){
+		openNav3();
+		openOffcanvas();
+	});
+	$("#myOffcanvas .closeOffcanvas").click(function(){
+		closeOffcanvas();
+	});
+	$("#myOffcanvas a").click(function(){
+		// ici on lance l'action que l'on veut et on ferme le menu
+		closeOffcanvas();
+	});
+	$("#myCanvasNav").click(function(){
+		closeOffcanvas();
+	});
+	/* END https://www.w3schools.com/howto/howto_js_off-canvas.asp */
+
 	var scoreSize = .8;
 	var keyUp_delay = 2000;
+	var maxTextareaLines = 15;
 	function updateABC() {
 		var input = $("#abc-text").val();
 		// var scoreSize = $("#abc-edit-slider-size").slider("option", "value") ;
@@ -44,13 +79,20 @@ $(document).ready(function(){
 		})();
 
 	// ABCJS.renderAbc('abc-score', $("#abc-text").val() , {}, {scale: scoreSize},{});
-	// for abcjs_editor params : https://github.com/paulrosen/abcjs/blob/master/api.md#abcjs-editor
+	
 	var updateABCTextArea = (function(){
 		abc = $("#abc-text").val();
 		lines = abc.split('\n');
-		$("textarea#abc-text").css("min-height", (lines.length * 1.5) + "em");
+		if (lines < maxTextareaLines) {
+			$("textarea#abc-text").css("min-height", (lines.length * 1.5) + "em");
+		} else {
+			$("textarea#abc-text").css("min-height", (maxTextareaLines * 1.5) + "em");
+		}
 	})
 	updateABCTextArea();
+	/*
+	 for abcjs_editor params : https://github.com/paulrosen/abcjs/blob/master/api.md#abcjs-editor
+	 */
 	abc_editor = new ABCJS.Editor("abc-text", { canvas_id: "abc-score", warnings_id: "abc-warnings"});
 	$("#abc-text").on("keyup",function(ev){
 		updateABCTextArea();
