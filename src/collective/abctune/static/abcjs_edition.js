d58434ce12fe544b617913a753023e8a24c6e996
Eric Hardy <Eric.Hardy@univ-brest.fr>
2017-12-12 19:07:05 +0100

    classes & misc menu

--- src/collective/abctune/static/abcjs_edition.js
+++ src/collective/abctune/static/abcjs_edition.js
@@ -4,37 +4,34 @@ $(document).ready(function(){
 	console.log('dans EDition');
 
 	/* https://www.w3schools.com/howto/howto_js_off-canvas.asp */
-	function openOffcanvas() {
-	    document.getElementById("myOffcanvas").style.width = "250px";
+	function openAbctuneMenu() {
+	    document.getElementById("actions-menu").style.width = "250px";
 	    document.getElementById("main-container").style.marginRight = "250px";
 	};
-	function openNav3() {
-	    document.getElementById("myCanvasNav").style.width = "100%";
-	    document.getElementById("myCanvasNav").style.opacity = "0.8";  
+	function openOverlay() {
+	    document.getElementById("menu-overlay").style.width = "100%";
+	    document.getElementById("menu-overlay").style.opacity = "0.8";  
 	};
-	function closeOffcanvas() {
-	    document.getElementById("myOffcanvas").style.width = "0%";
+	function closeAbctuneMenu() {
+	    document.getElementById("actions-menu").style.width = "0%";
 	    document.getElementById("main-container").style.marginRight= "0%";
 	    document.body.style.backgroundColor = "white";
-	    document.getElementById("myCanvasNav").style.width = "0%";
-	    document.getElementById("myCanvasNav").style.opacity = "0"; 
-	};
-	function closeOffcanvas2() {
-	    document.getElementById("myOffcanvas2").style.display = "none";
+	    document.getElementById("menu-overlay").style.width = "0%";
+	    document.getElementById("menu-overlay").style.opacity = "0"; 
 	};
 	$("#dashboard").click(function(){
-		openNav3();
-		openOffcanvas();
+		openOverlay();
+		openAbctuneMenu();
 	});
-	$("#myOffcanvas .closeOffcanvas").click(function(){
-		closeOffcanvas();
+	$("#actions-menu .closeMenu").click(function(){
+		closeAbctuneMenu();
 	});
-	$("#myOffcanvas a").click(function(){
+	$("#actions-menu a").click(function(){
 		// ici on lance l'action que l'on veut et on ferme le menu
-		closeOffcanvas();
+		closeAbctuneMenu();
 	});
-	$("#myCanvasNav").click(function(){
-		closeOffcanvas();
+	$("#menu-overlay").click(function(){
+		closeAbctuneMenu();
 	});
 	/* END https://www.w3schools.com/howto/howto_js_off-canvas.asp */
 
