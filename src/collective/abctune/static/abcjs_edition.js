8951bf162e67c8ae303995904714e0599609a275
Eric Hardy <Eric.Hardy@univ-brest.fr>
2017-12-14 00:45:36 +0100

    ajax views

--- src/collective/abctune/static/abcjs_edition.js
+++ src/collective/abctune/static/abcjs_edition.js
@@ -9,11 +9,22 @@ $(document).ready(function(){
 		$("#edit-abc").addClass('btn-info');
 		$.ajaxSetup({ cache: false });
 		abctext = $("#abc-text").val();
-		var updatedABC = $.getJSON("@@updateABC" , {'abctext':abctext, 'uuid':uuid, 'makeSound':1} , function(data, status, xhr){
+		var updatedABC = $.post("@@updateABC" , {'abctext':abctext, 'uuid':uuid, 'makeSound':1} , function(data, status, xhr){
 			console.log("retour de updateABC");
-			// console.log(status);
-			var obj = JSON.parse(data);
-			// console.log(data.svg);
+			var svg = $.post("@@getSVG", {'uuid':uuid}, function(data){
+				console.log(data);
+				$("#svgscore").html(data);
+			});
+			var pdf = $.post("@@getPDF", {'uuid':uuid}, function(data){
+				console.log(data);
+				$("#pdf").html(data);
+			});
+			var ogg = $.post("@@getOGG", {'uuid':uuid}, function(data){
+				console.log(data);
+				$("#ogg").html(data);
+			});
+
+			
 		});
 	})
 
