7e65a61ebf0e8604fe42ef77f02411658a41cbc5
Eric Hardy <Eric.Hardy@univ-brest.fr>
2018-01-09 00:07:21 +0100

    must return ogg/pdfscore/svgscore ok or None

--- src/collective/abctune/browser/get_ogg.py
+++ src/collective/abctune/browser/get_ogg.py
@@ -14,13 +14,16 @@ class getOGG(BrowserView):
     def __call__(self, uuid):
         # need to remove 'view' at the end if present
         abc = uuidToObject(uuid)
-        ogg = '<audio controls>'
-        ogg += '<source type="audio/ogg" src="' + abc.absolute_url()
-        ogg += '/@@download/ogg/'
-        ogg += abc.ogg.filename
-        # ogg += '?' + str(microsecond) + '">'
-        ogg += '">'
-        ogg += u'Your browser does not support the audio element.'
-        ogg += '</audio>'
-        # logger.info(ogg)
-        return ogg
+        try:
+            ogg = '<audio controls>'
+            ogg += '<source type="audio/ogg" src="' + abc.absolute_url()
+            ogg += '/@@download/ogg/'
+            ogg += abc.ogg.filename
+            # ogg += '?' + str(microsecond) + '">'
+            ogg += '">'
+            ogg += u'Your browser does not support the audio element.'
+            ogg += '</audio>'
+            # logger.info(ogg)
+            return ogg
+        except Exception:
+            return None
