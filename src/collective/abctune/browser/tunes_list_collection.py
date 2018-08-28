1e5fef2a6fcd606c1c399342bb726f50d5691876
Eric Hardy <Eric.Hardy@univ-brest.fr>
2018-08-29 00:39:57 +0200

    tunes collection view

--- src/collective/abctune/browser/tunes_list_collection.py
+++ src/collective/abctune/browser/tunes_list_collection.py
@@ -1,24 +1,36 @@
 # -*- coding: utf-8 -*-
 
-from plone.app.uuid.utils import uuidToObject
-from zope.annotation.interfaces import IAnnotations
-from zope.publisher.browser import BrowserView
-
+from plone.app.contenttypes.browser.collection import CollectionView
 import logging
 
 
-logger = logging.getLogger('collective.abctune:getOUTPUTS ')
+logger = logging.getLogger('collective.abctune:tunesListCollection ')
 
 
-class tunesListCollection(BrowserView):
-    """ AJAX method/view"""
-    def __call__(self, uuid, key):
-        abc = uuidToObject(uuid)
-        annot = IAnnotations(abc)
-        try:
-            output = annot.get(key)
-            # logger.info(key)
-            # logger.info(output)
-            return '<pre>' + output + '</pre>'
-        except Exception:
-            return ''
+class tunesListCollection(CollectionView):
+    """
+    code from plone.app.contenttypes.browser.collection.CollectionView
+    """
+    def results(self, **kwargs):
+        """Return a content listing based result set with results from the
+        collection query.
+
+        :param **kwargs: Any keyword argument, which can be used for catalog
+                         queries.
+        :type  **kwargs: keyword argument
+
+        :returns: plone.app.contentlisting based result set.
+        :rtype: ``plone.app.contentlisting.interfaces.IContentListing`` based
+                sequence.
+        """
+        # Extra filter
+        contentFilter = self.request.get('contentFilter', {})
+        contentFilter.update(kwargs.get('contentFilter', {}))
+        kwargs.setdefault('custom_query', contentFilter)
+        kwargs.setdefault('batch', True)
+        kwargs.setdefault('b_size', self.b_size)
+        kwargs.setdefault('b_start', self.b_start)
+
+        results = self.collection_behavior.results(**kwargs)
+        # here, we return a list of objects.
+        return [obj.getObject() for obj in results]
